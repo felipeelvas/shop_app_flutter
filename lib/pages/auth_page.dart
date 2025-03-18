4431726062015a3665b57ef5620c18c1c9e8835c
Felipe Elvas Barbosa <elvasfelipe@hotmail.com>
2025-03-17 22:10:28 -0300

    correção de responsividade

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -24,47 +24,51 @@ class AuthPage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 70,
-                  ),
-                  //cascade operator
-                  transform: Matrix4.rotationZ((-8 * pi / 180))
-                    ..translate(-10.0),
-                  // Note the use of cascade operator (..) Apply translation after rotation
-
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.deepOrange.shade900,
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 8,
-                        offset: Offset(0, 2),
+          Center(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 70,
                       ),
-                    ],
-                  ),
-                  child: Text(
-                    'Minha Loja',
-                    style: TextStyle(
-                        fontSize: 45,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Anton',
-                        color: Theme.of(context).colorScheme.onPrimary),
-                    textAlign: TextAlign.center,
-                  ),
+                      //cascade operator
+                      transform: Matrix4.rotationZ((-8 * pi / 180))
+                        ..translate(-10.0),
+                      // Note the use of cascade operator (..) Apply translation after rotation
+
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.deepOrange.shade900,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 8,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: Text(
+                        'Minha Loja',
+                        style: TextStyle(
+                            fontSize: 45,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Anton',
+                            color: Theme.of(context).colorScheme.onPrimary),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    AuthForm(),
+                  ],
                 ),
-                const SizedBox(height: 20),
-                AuthForm(),
-              ],
+              ),
             ),
           )
         ],
